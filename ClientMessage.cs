b34a60b577bd9681fc8f2fe71ff67708ee14cf22
dncfjy <dncfjy2013@163.com>
2025-04-10 17:25:37 +0800

    补充

---
+++ ClientMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ClientMessage
+    {
+        public ClientConfig Client { get; set; }
+        public byte[] Data { get; set; }
+        public DateTime ReceivedTime { get; set; }
+    }
+}
