9b3fce3fad2b77c8edc1c524af023e7a244cfc85
dncfjy <dncfjy2013@163.com>
2025-05-20 15:47:14 +0800

    补充外部变量

---
+++ Proxy/Common/ProxyConstant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.Common
+{
+    public class ProxyConstant
+    {
+        public static bool _isUseUDPMap = false;
+        public static bool _isUseTCPMap = false;
+        public static bool _isUseHttpMap = false;
+    }
+}
